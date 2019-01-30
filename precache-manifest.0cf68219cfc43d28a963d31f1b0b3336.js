780111839fad26c79f4cc576083f8ea8b3b6c013
sushantc10 <haldonkar.sushant@gmail.com>
2019-01-30 22:42:02 +0530

    push again

---
+++ precache-manifest.0cf68219cfc43d28a963d31f1b0b3336.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/catchfish/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "7ebd199b28619304c1e1",
+    "url": "/catchfish/static/css/main.7c65dfb2.chunk.css"
+  },
+  {
+    "revision": "3c2e61d4cb3a1ac79d2e",
+    "url": "/catchfish/static/js/1.3c2e61d4.chunk.js"
+  },
+  {
+    "revision": "8637df83dde11545e901",
+    "url": "/catchfish/static/js/runtime~main.8637df83.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/catchfish/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/catchfish/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/catchfish/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "7ebd199b28619304c1e1",
+    "url": "/catchfish/static/js/main.7ebd199b.chunk.js"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/catchfish/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/catchfish/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/catchfish/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/catchfish/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/catchfish/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "332b40b38c5c6457a2d57fd88c0da8ec",
+    "url": "/catchfish/index.html"
+  }
+];
